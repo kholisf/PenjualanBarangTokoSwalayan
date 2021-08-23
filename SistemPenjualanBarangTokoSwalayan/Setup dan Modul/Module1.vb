b6353f81348669cf491d0f1d191595d434a1a976
kholisf <kholisfikri01@gmail.com>
2021-08-23 21:35:09 +0800

    upload perbaikan sebelum revisi

--- SistemPenjualanBarangTokoSwalayan/Setup dan Modul/Module1.vb
+++ SistemPenjualanBarangTokoSwalayan/Setup dan Modul/Module1.vb
@@ -1,9 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 Imports DevExpress.LookAndFeel
 Imports DevExpress.XtraBars.Helpers
-
 Module Module1
-
     Public Function EksekusiQuery(ByVal QueryString As String) As DataTable
         Dim Connection As New MySqlConnection(My.Settings.sKoneksi)
         Dim DataAdapter As New MySqlDataAdapter(QueryString, Connection)
@@ -11,14 +9,9 @@ Module Module1
         DataAdapter.Fill(DataTable)
         Return DataTable
     End Function
-
-
-
     Public Function KodeOtomatis(ByVal NamaTabel As String, ByVal NamaKolom As String, ByVal KodeInisial As String, ByVal DigitFormat As String) As String
         Dim KodeText As String = ""
-
         Dim DataKode = EksekusiQuery("select max(right(" & NamaKolom & "," & DigitFormat.Length & ")) as " & NamaKolom & " from " & NamaTabel & "").Select()
-
         Try
             KodeText = KodeInisial & Format(DataKode(0).Item(NamaKolom) + 1, DigitFormat)
         Catch ex As Exception
@@ -26,54 +19,35 @@ Module Module1
         End Try
         Return KodeText
     End Function
-
-
     'Panggil combobox dari database
     Public Sub ComboboxTambahItem(ByVal NamaTabel As String, ByVal NamaKolom As String, ByVal NamaKomboBox As DevExpress.XtraEditors.ComboBoxEdit)
         NamaKomboBox.Properties.Items.Clear()
         Dim cari = EksekusiQuery("Select distinct(" & NamaKolom & ") as " & NamaKolom & " from " & NamaTabel & "").Select()
-
-
         For i = 0 To cari.Length - 1
             NamaKomboBox.Properties.Items.Add(cari(i).Item(NamaKolom))
         Next
     End Sub
-
     Sub barbuttonTampilOrTidak(ByVal namakolon As String, ByVal ParamArray componentname() As Object)
         Dim cari = EksekusiQuery("select * from hak_akses where NamaAkses='" & My.Settings.sHakAkses & "'").Select()
         Dim sp = namakolon.Split(",")
-
         Dim ii As Integer = 0
-        
         For Each i In sp
-
             With cari(0)
                 If .Item(i) <> 1 Then
                     componentname(ii).Visibility = DevExpress.XtraBars.BarItemVisibility.Never
                 Else
                     componentname(ii).Visibility = DevExpress.XtraBars.BarItemVisibility.Always
                 End If
-
             End With
             ii += 1
-
-
-
         Next
-
-
     End Sub
-
-
-
     Public Sub GridviewEditTampilan(ByVal NamaGridView As DevExpress.XtraGrid.Views.Grid.GridView, Optional ByVal SembunyiKolom As String = Nothing, Optional ByVal FormatKolomKeNumeric As String = Nothing, Optional ByVal SumKolomDanMenampilkanDibawahGridview As String = Nothing, Optional ByVal DenyEditFieldNames As String = Nothing)
-        
         With NamaGridView
             If SembunyiKolom <> Nothing Then
                 Dim kolom = SembunyiKolom.Split(",")
                 For Each i In kolom
                     .Columns(i).Visible = False
-
                 Next
             End If
             If FormatKolomKeNumeric <> Nothing Then
@@ -86,11 +60,9 @@ Module Module1
             If SumKolomDanMenampilkanDibawahGridview <> Nothing Then
                 .OptionsView.ShowFooter = True
                 Dim kolom = SumKolomDanMenampilkanDibawahGridview.Split(",")
-
                 For Each i In kolom
                     .Columns(i).SummaryItem.SetSummary(DevExpress.Data.SummaryItemType.Sum, "{0:n" & Val(i) & "}")
                 Next
-
             End If
             If DenyEditFieldNames <> Nothing Then
                 Dim FieldName = DenyEditFieldNames.Split(",")
@@ -98,14 +70,7 @@ Module Module1
                     .Columns(i).OptionsColumn.AllowEdit = False
                 Next
             End If
-
-
             .BestFitColumns()
         End With
-
-
     End Sub
-
-
-
 End Module
--- SistemPenjualanBarangTokoSwalayan/Setup dan Modul/ReportModule.vb
+++ SistemPenjualanBarangTokoSwalayan/Setup dan Modul/ReportModule.vb
@@ -1,6 +1,5 @@
 ﻿Imports DevExpress.XtraReports.UI
 Module ReportModule
-
     Sub ReportNotaPenjualan2(ByVal NomorTransaksi As String, ByVal Tanggal As Date, ByVal JumlahUang As Double, ByVal Kembalian As Double, ByVal StatusCetak As Boolean, ByVal potongan As Double, ByVal totalsemua As Double, ByVal kodekonsumen As String)
         EksekusiQuery("create or replace view view_noteuntukkonsumen as select a.KodeBarang,b.NamaBarang,a.HargaSatuan,a.Jumlah,a.PersenDiskon,(a.HargaSatuan*a.Jumlah) as SubTotal,(a.HargaSatuan*a.Jumlah*(a.PersenDiskon/100)) as JumlahDiskon,(a.HargaSatuan*a.Jumlah)-(a.HargaSatuan*a.Jumlah*(a.PersenDiskon/100)) as JumlahHarga from (penjualan_detil a inner join barang b on a.KodeBarang=b.KodeBarang) where a.NomorTransaksi='" & NomorTransaksi & "' group by a.NomorTransaksi")
         Dim rpt As New rptNotePenjualan
@@ -19,27 +18,7 @@ Module ReportModule
                 PrintTool.Print()
             End If
         End Using
-
     End Sub
-    'Sub ReportNotaPenjualan(ByVal NomorTransaksi As String, ByVal Tanggal As Date, ByVal JumlahUang As Double, ByVal Kembalian As Double, ByVal StatusCetak As Boolean, ByVal potongan As Double, ByVal totalsemua As Double)
-    '    Dim rpt As New rptNotePenjualan
-    '    'rpt.FilterString = "[NomorTransaksi]='" & NomorTransaksi & "'"
-    '    rpt.FilterString = "[NomorTransaksi]='" & NomorTransaksi & "'"
-    '    rpt.txtNomorTransaksi.Text = " " & NomorTransaksi
-    '    rpt.txtTanggal.Text = " " & Tanggal
-    '    rpt.tblKembalian.Text = " " & Format(Kembalian, "n0")
-    '    rpt.tblPotongan.Text = " " & Format(potongan, "n0")
-    '    rpt.tblJumlahUang.Text = " " & Format(JumlahUang, "n0")
-    '    rpt.tblTotalSemua.Text = " " & Format(totalsemua, "n0")
-    '    Using PrintTool As New ReportPrintTool(rpt)
-    '        If StatusCetak = True Then
-    '            PrintTool.ShowRibbonPreviewDialog()
-    '        Else
-    '            PrintTool.Print()
-    '        End If
-    '    End Using
-
-    'End Sub
     Sub ReportLaporanInformasiBarang(ByVal kondisisql As String, ByVal Kondisilaporan As String, ByVal kondisiurut As String)
         EksekusiQuery("create or replace view view_informasibarang as select * from barang where " & kondisisql & " and KodeBarang<>'0000000000' order by " & kondisiurut & " asc")
         Dim rpt As New rptLaporanDataBarang
@@ -48,7 +27,6 @@ Module ReportModule
         Using PrintTool As New ReportPrintTool(rpt)
             PrintTool.ShowRibbonPreviewDialog()
         End Using
-
     End Sub
     Sub ReportLaporanPenjualan(ByVal tanggalawal As Date, ByVal tanggalakhir As Date)
         EksekusiQuery("create or replace view view_laporanpenjualan as select a.NomorTransaksi,a.Tanggal,a.Potongan,count(b.KodeBarang) as JumlahItem,sum((b.HargaSatuan*b.Jumlah) -(b.HargaSatuan*b.Jumlah)*(b.PersenDiskon/100)) as TotalPenjualan,(sum((b.HargaSatuan*b.Jumlah)-(b.HargaSatuan*b.Jumlah)*(b.PersenDiskon/100))-a.Potongan) as TotalPembayaran,a.JumlahUang,a.KodePegawai from (penjualan a inner join penjualan_detil b on a.NomorTransaksi=b.NomorTransaksi) where a.Tanggal>='" & Format(tanggalawal, "yyyy-MM-dd 00:00:00") & "' and a.Tanggal<='" & Format(tanggalakhir, "yyyy-MM-dd HH:mm:ss") & "' group by a.NomorTransaksi")
@@ -58,9 +36,7 @@ Module ReportModule
         Using PrintTool As New ReportPrintTool(rpt)
             PrintTool.ShowRibbonPreviewDialog()
         End Using
-
     End Sub
-
     Sub ReportLaporanPembelianPertanggal(ByVal tanggalawal As Date, ByVal tanggalakhir As Date)
         EksekusiQuery("create or replace view view_laporanpembelianpertanggal as select a.Tanggal,a.NomorTransaksi,a.KodePemasok,b.NamaPemasok,count(c.KodeBarang) as JumlahItem,a.Potongan,sum((c.HargaSatuan*c.Jumlah) -(c.HargaSatuan*c.Jumlah)*(c.PersenDiskon/100)) as TotalPembelian,(sum((c.HargaSatuan*c.Jumlah)-(c.HargaSatuan*c.Jumlah)*(c.PersenDiskon/100))-a.Potongan) as TotalPembayaran,b.Keterangan,a.KodePegawai from (pembelian a inner join pemasok b on a.KodePemasok=b.KodePemasok inner join pembelian_detil c on a.NomorTransaksi=c.NomorTransaksi) where a.Tanggal>='" & Format(tanggalawal, "yyyy-MM-dd 00:00:00") & "' and a.Tanggal<='" & Format(tanggalakhir, "yyyy-MM-dd HH:mm:ss") & "' group by a.NomorTransaksi")
         Dim rpt As New rptLaporanPembelianPerTanggal
@@ -74,7 +50,6 @@ Module ReportModule
         EksekusiQuery("create or replace view view_laporanpembelianperpemasok as select a.Tanggal,a.NomorTransaksi,a.KodePemasok,b.NamaPemasok,count(c.KodeBarang) as JumlahItem,a.Potongan,sum((c.HargaSatuan*c.Jumlah) -(c.HargaSatuan*c.Jumlah)*(c.PersenDiskon/100)) as TotalPembelian,(sum((c.HargaSatuan*c.Jumlah)-(c.HargaSatuan*c.Jumlah)*(c.PersenDiskon/100))-a.Potongan) as TotalPembayaran,b.Keterangan,a.KodePegawai from (pembelian a inner join pemasok b on a.KodePemasok=b.KodePemasok inner join pembelian_detil c on a.NomorTransaksi=c.NomorTransaksi) where a.KodePemasok='" & kodepemasok & "' group by a.NomorTransaksi")
         Dim rpt As New rptLaporanPembelianPerPermasok
         rpt.txtTanggalAwal.Text = " " & namapemasok & " (" & kodepemasok & ") "
-
         Using PrintTool As New ReportPrintTool(rpt)
             PrintTool.ShowRibbonPreviewDialog()
         End Using
@@ -88,7 +63,6 @@ Module ReportModule
         Using PrintTool As New ReportPrintTool(rpt)
             PrintTool.ShowRibbonPreviewDialog()
         End Using
-
     End Sub
     Sub ReportLaporanPembelianDetil(ByVal nomorTransaksi As String, ByVal tanggal As Date, ByVal namapegawai As String, ByVal namapemasok As String)
         EksekusiQuery("create or replace view view_laporanpembeliandetil as select a.KodeBarang,b.NamaBarang,a.HargaSatuan,a.Jumlah,(a.HargaSatuan*a.Jumlah) as SubTotalHarga,a.PersenDiskon,((a.HargaSatuan*a.Jumlah) *(PersenDiskon/100))as JumlahDiskon,((a.HargaSatuan*a.Jumlah)-((a.HargaSatuan*a.Jumlah) *(PersenDiskon/100))) as TotalHarga from (pembelian_detil a inner join barang b on a.Kodebarang=b.Kodebarang) where a.NomorTransaksi='" & nomorTransaksi & "'  order by Namabarang asc")
@@ -100,9 +74,7 @@ Module ReportModule
         Using PrintTool As New ReportPrintTool(rpt)
             PrintTool.ShowRibbonPreviewDialog()
         End Using
-
     End Sub
-
     Sub ReportlaporanKas(ByVal saldo As String, ByVal tanggalawal As Date, ByVal tanggalakhir As Date)
         EksekusiQuery("create or replace view view_laporanKas as select Tanggal,NomorTransaksi,Uraian,Masuk,Keluar,Saldo,KodePegawai from kas where Tanggal>='" & Format(tanggalawal, "yyyy-MM-dd ") & " 00:00:00' and Tanggal<='" & Format(tanggalakhir, "yyyy-MM-dd HH:mm:ss") & "'")
         Dim rpt As New rptLaporanKas2
